d9ab30553a884b2f406688c8aa5c04947bf9a8ba
azbyn <andreypavel1@yahoo.com>
2018-03-01 22:53:33 +0200

    updated clang-format

--- rect.h
+++ rect.h
@@ -15,11 +15,11 @@ public:
     inline Point P0() const { return {X0(), Y0()}; }
     inline Point P1() const { return {X1(), Y1()}; }
 
-    inline bool operator== (const Rect& rhs) const {
-        return X0() == rhs.X0()
-            && Y0() == rhs.Y0()
-            && X1() == rhs.X1()
-            && Y1() == rhs.Y1();
+    inline bool operator==(const Rect& rhs) const {
+        return X0() == rhs.X0() &&
+               Y0() == rhs.Y0() &&
+               X1() == rhs.X1() &&
+               Y1() == rhs.Y1();
     }
 };
 
@@ -30,7 +30,8 @@ class RectWH : public Rect {
     int h = 0;
 
 public:
-    constexpr RectWH(int x, int y, int width, int height) : x(x), y(y), w(width), h(height) {}
+    constexpr RectWH(int x, int y, int width, int height)
+        : x(x), y(y), w(width), h(height) {}
     inline int X() const override { return x; }
     inline int Y() const override { return y; }
     inline int Width() const override { return w; }
@@ -45,8 +46,10 @@ class Rect4 : public Rect {
     int y1 = 0;
 
 public:
-    constexpr Rect4(int x0, int y0, int x1, int y1) : x0(x0), y0(y0), x1(x1), y1(y1) {}
-    constexpr Rect4(Point p0, Point p1) : x0(p0.x), y0(p0.y), x1(p1.x), y1(p1.y) {}
+    constexpr Rect4(int x0, int y0, int x1, int y1)
+        : x0(x0), y0(y0), x1(x1), y1(y1) {}
+    constexpr Rect4(Point p0, Point p1)
+        : x0(p0.x), y0(p0.y), x1(p1.x), y1(p1.y) {}
     inline int X() const override { return x0; }
     inline int Y() const override { return y0; }
     inline int Width() const override { return x1 - x0 + 1; }
@@ -54,4 +57,5 @@ public:
     inline int X1() const override { return x1; }
     inline int Y1() const override { return y1; }
 };
+
 } // namespace azbyn
